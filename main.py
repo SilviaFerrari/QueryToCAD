86ce6417d3f2e1f9b70bc1871bcb5dfb4b197816
Silvia Ferrari <silvia.ferrari369@gmail.com>
2026-01-23 10:58:55 +0100

    Added analyze_geometry() to check the validity of an object.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ LLM_MODELS = [
 
 EXCEL_FILE = "output/performance.xlsx"
 
+# function to manage excel file (creation/reading/writing/errors)
 def save_to_excel(obj_data):
 
     # pass the object data dictionary into a list to prevent pandas from getting confused
@@ -49,8 +50,46 @@ def save_to_excel(obj_data):
         except Exception as e:
             print(f"An unexpected error occurred while saving file: {e}")
             break
-        
+
     print("UNABLE TO SAVE TO EXCEL. The data from this run is lost or only printed to screen.")
+
+# function to analyze the object to determine if it's valid
+def analyze_geometry(cq_object):
+    
+    total_volume = 0
+    total_faces = 0
+    
+    try:
+        obj_to_analyze = []
+        
+        # checking whether cq_object it's a workplane or not 
+        if isinstance(cq_object, cq.Workplane):
+            # extracts the list of actual solid objects from the container
+            obj_to_analyze = cq_object.vals() 
+        else:
+            # if it's not a workplane, we assume that it's a geometric object
+            obj_to_analyze = [cq_object] 
+
+        for obj in obj_to_analyze:
+
+            # checking and calculating the total volume
+            if hasattr(obj, "Volume"):
+                obj_vol = obj.Volume()
+                total_volume += obj_vol
+
+            # checking and calculating the number of faces
+            if hasattr(obj, "Faces"):
+                obj_faces = len(obj.Faces())
+                total_faces += obj_faces
+        
+    except Exception as e:
+        print(f"An error occurred during the geometrical analysis: {e}")
+        return {"volume": 0, "faces": 0}
+
+    return {
+        "volume": total_volume, 
+        "faces": total_faces
+    }
 
 # creating output folder
 if not os.path.exists(f"output/{AI_MODEL}"):
